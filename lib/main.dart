558fcacf06e941c9f171477d84c3e77766e603f1
Utkarsh Verma <utkarsh1765uv7@gmail.com>
2026-07-23 22:28:11 +0530

    created a stateful profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,18 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'screens/profile_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text("Student ID Card"),
-        ),
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 350,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(
-                color: Colors.blue,
-                width:2,
-              )
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Colors.blue,
-                  child: Icon(Icons.person, size: 80, color: Colors.white),
-                ),
-                Text(
-                  "Utkarsh Verma",
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "BTech CSE",
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(fontSize: 20, color: Colors.blue),
-                ),
-                Text(
-                  "Roll No.:2415001713",
-                  textDirection:TextDirection.ltr,
-                  style: TextStyle(fontSize: 20),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+      home: const ProfileScreen(),
+    );
+  }
 }
